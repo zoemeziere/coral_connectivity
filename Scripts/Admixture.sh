a02602c70e035f35f5e8f5eefd591e57fcbb13af
Zoe Meziere <71752811+zoemeziere@users.noreply.github.com>
2024-03-20 14:41:42 +1000

    Create Admixture.sh

---
+++ Scripts/Admixture.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=20
+#SBATCH --job-name=admixture
+#SBATCH --time=00:10:00
+#SBATCH --account=a_riginos
+#SBATCH --partition=general
+
+source /QRISdata/Q5253/miniconda3/etc/profile.d/conda.sh
+conda activate admixture
+
+FILE=StyloTaxon1_SF095_LD
+
+awk '{$1="0";print $0}' $FILE.bim > $FILE.bim.tmp
+mv $FILE.bim.tmp $FILE.bim
+
+for i in {2..15}
+do
+ admixture --cv $FILE.bed $i > log${i}.out 
+done
+
+awk '/CV/ {print $3,$4}' *out | cut -c 4,7-20 > $FILE.cv.error
