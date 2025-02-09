a1be71951dbe1dbb016bc37cb99f4a8884e49687
Zoe Meziere <71752811+zoemeziere@users.noreply.github.com>
2025-02-09 12:50:26 +1000

    Update genepop_Pver.R

--- Scripts/genepop_Pver.R
+++ Scripts/genepop_Pver.R
@@ -1,3 +1,3 @@
 library(genepop)
 
-PverTaxon1A_IBD_10000 <- ibd(inputFile= "PverTaxon1A_genepop_mercator.txt", outputFile = "PverTaxon1A_genepop_1d_All_out.txt", statistic='a', dataType='Diploid', settingsFile = '', geographicScale='1D', verbose = interactive())
+PverTaxon1A_IBD_10000 <- ibd(inputFile= "Pver_genepop_mercator.txt", outputFile = "Pver_genepop_1d_out.txt", statistic='a', dataType='Diploid', settingsFile = '', geographicScale='1D', verbose = interactive())
