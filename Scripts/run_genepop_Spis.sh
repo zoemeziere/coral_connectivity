49388bb91f3aced2dcf4b5bb0b9dfa3ddf096d45
Zoe Meziere <71752811+zoemeziere@users.noreply.github.com>
2025-02-09 12:50:43 +1000

    Update run_genepop_Spis.sh

--- Scripts/run_genepop_Spis.sh
+++ Scripts/run_genepop_Spis.sh
@@ -1,7 +1,7 @@
 library(genepop)
 
-StyloTaxon1_IBD_all_test <- ibd(inputFile= "StyloTaxon1_genepop.txt", 
-        outputFile = "StyloTaxon1_genepop_out.txt", 
+StyloTaxon1_IBD_all_test <- ibd(inputFile= "Spis_genepop.txt", 
+        outputFile = "Spis_genepop_out.txt", 
         statistic='a', 
         dataType='Diploid', 
         settingsFile = '', 
